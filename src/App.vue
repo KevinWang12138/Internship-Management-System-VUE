c9201e760c9656d236d77e895ba4fa721fb784e0
KevinWang12138 <wangqianming12138@gmail.com>
2023-03-04 23:45:41 +0800

    删除无用组件，配置app组件置顶，创建login组件

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import NotFound from './components/NotFound.vue'
+import Login from './components/login/Login.vue'
+const routes = {
+  '/login': Login
+}
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash,
+      divClass: 'navigation'
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+    })
+  }
+}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div :class="divClass">
+    <a href="#/login">登录</a> | <a href="#/non-existent-path">Broken Link</a>
+  </div>
+  <br>
+  <component :is="currentView" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
+.navigation {
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
