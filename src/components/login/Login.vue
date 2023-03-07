021b5b1cc7abc2b289531d9da727329ffb2b875d
KevinWang12138 <wangqianming12138@gmail.com>
2023-03-07 19:38:24 +0800

    登录界面基础html完成

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>登录</h1>
+  <div>
+    <input id="phone" type="text" value="手机号码" @focusin="changeDefaultPhoneValue()" @focusout="changeDefaultPhoneValue()">
+    <br>
+    <input id="password" type="password">
+    <br>
+    <button>登录</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  methods:{
+    changeDefaultPhoneValue(){
+      const phone = document.getElementById("phone");
+      if(phone.value == ""){
+        phone.value = "手机号码"
+      }else if(phone.value == "手机号码"){
+        phone.value = ""
+      }
+    }
+  }
 }
 </script>
 
