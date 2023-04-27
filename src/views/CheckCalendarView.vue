4aac6a7b89af83045ddeef614f7d73d9a1e0daaf
KevinWang12138 <wangqianming12138@gmail.com>
2023-04-28 00:35:33 +0800

    查看学生日历基础template

--- src/views/CheckCalendarView.vue
+++ src/views/CheckCalendarView.vue
@@ -1,11 +1,122 @@
 <template>
-1
+  <el-select v-model="value" class="m-2" placeholder="选择学生" size="large">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    />
+  </el-select>
+  <el-calendar>
+    <template #dateCell="{data}">
+      <div class="calendar-item">
+        <div class="calendar-time">
+          {{ data.day.split('-').slice(2).join('')}}
+        </div>
+        <div>
+          <span class="remark-text calendar-time" v-for="(item, index) in dealMyDate(data.day)" :key="index">
+            {{ item }}
+          </span>
+        </div>
+      </div>
+    </template>
+  </el-calendar>
 </template>
 
-<script>
-export default {
-  name: "CheckCalendarView"
-}
+<script lang="ts">
+import {defineComponent, reactive, ref} from "vue";
+import {getBasicCalendarInfo} from "@/request/api";
+export default defineComponent({
+  name:"CheckApplicationView",
+  setup(){
+    const resDate = reactive([
+      {date: '', content: ''},
+    ]);
+    //访问后端得到日历信息
+    //从后端获得日历信息，key是date，content由两部分组成，实习单位：工作日
+    //todo 补充休息日
+    //todo 额外获取额外信息，补充请假等信息
+    getBasicCalendarInfo({student_id:localStorage.getItem("phone")?.toString()}).then(res=>{
+      //res返回
+      console.log(res)
+      const companyNames=res.data.company_names
+      const endDates=res.data.end_dates
+      const startDates=res.data.start_dates
+      for (let index=0;index<companyNames.length;index++){
+        let startDate=startDates[index]
+        let endDate=endDates[index]
+
+        let info=companyNames[index]+"实习 工作日"
+
+        let myDate=new Date(startDate);
+        let end=new Date(endDate);
+        while(myDate<=end){
+          let d=""
+          d+=myDate.getFullYear()
+          d+="-"
+          let month=myDate.getMonth()+1
+          if(month.toString().length==1){
+            d+="0"
+            d+=month
+          }else{
+            d+=month
+          }
+          d+="-"
+          let day=myDate.getDate()
+          if(day.toString().length==1){
+            d+="0"
+            d+=myDate.getDate()
+          }else {
+            d+=myDate.getDate()
+          }
+          resDate.push({date: d,content: info})
+          myDate.setDate(myDate.getDate()+1)
+        }
+      }
+    })
+
+
+    const dealMyDate = (v: string) => {
+      let res = '';
+      for (let index = 0; index < resDate.length; index++) {
+        if (resDate[index].date == v) {
+          res = resDate[index].content;
+          break;
+        }
+      }
+      return res;
+    }
+    return {value, options, dealMyDate}
+  },
+  components:{
+
+  },
+})
+
+const value = ref('')
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
 </script>
 
 <style scoped>
