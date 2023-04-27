4c7bd2949bae78a16b700f07420833ed6d321601
KevinWang12138 <wangqianming12138@gmail.com>
2023-04-28 00:29:02 +0800

    教师审批界面基本template完成

--- src/views/CheckApplication.vue
+++ src/views/CheckApplication.vue
@@ -1,13 +1,79 @@
 <template>
-1
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="150" />
+    <el-table-column prop="name" label="Name" width="120" />
+    <el-table-column prop="state" label="State" width="120" />
+    <el-table-column prop="city" label="City" width="120" />
+    <el-table-column prop="address" label="Address" width="600" />
+    <el-table-column prop="zip" label="Zip" width="120" />
+    <el-table-column fixed="right" label="Operations" width="120">
+      <template #default>
+        <el-button link type="primary" size="small" @click="handleClick"
+        >Detail</el-button
+        >
+        <el-button link type="primary" size="small">Edit</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  <!--底部的分页-->
+  <el-pagination background layout="prev, pager, next" :total="1000"/>
 </template>
 
-<script>
-export default {
-  name: "CheckApplication"
+<script lang="ts">
+import {defineComponent, reactive, ref} from "vue";
+export default defineComponent({
+  name:"CheckApplicationView",
+  setup(){
+    return {handleClick, tableData}
+  },
+  components:{
+
+  },
+})
+
+const handleClick = () => {
+  console.log('click')
 }
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+]
 </script>
 
 <style scoped>
-
 </style>
