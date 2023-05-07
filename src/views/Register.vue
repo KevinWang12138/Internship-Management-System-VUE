07b70b06d79ef33346cbc452a8a29af926bb09e6
KevinWang12138 <wangqianming12138@gmail.com>
2023-05-07 20:33:20 +0800

    完成注册后跳转登录界面逻辑

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -36,63 +36,73 @@
   </el-form>
 </template>
 
-<script lang="ts" setup>
-import { reactive, ref } from 'vue'
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
 import type { FormInstance, FormRules } from 'element-plus'
-import { register } from "@/request/api";
-import router from "@/router";
+import { login, register } from "@/request/api";
+import { useRouter } from "vue-router";
+import { LoginData } from "@/type/login";
 
-const formSize = ref('default')
-const ruleFormRef = ref<FormInstance>()
-const ruleForm = reactive({
-  name: '',
-  phone: '',
-  password: '',
-  school: '',
-  type: '',
-})
 
-const rules = reactive<FormRules>({
-  name: [
-    { required: true, message: '请输入姓名', trigger: 'blur' }
-  ],
-  phone: [
-    { required: true, message: '请输入手机号', trigger: 'blur' },
-    { min: 11, max: 11, message: '请输入11位手机号', trigger: 'blur' },
-  ],
-  password: [
-    { required: true, message: '请输入密码', trigger: 'blur' }
-  ],
-  school: [
-    { required: true, message: '请输入学校名称', trigger: 'blur' }
-  ],
-  type: [
-    { required: true, message: '必须选择一个类型', trigger: 'blur' }
-  ],
-})
+export default defineComponent({
+  setup(){
+    const router = useRouter()
+    const formSize = ref('default')
+    const ruleFormRef = ref<FormInstance>()
+    const ruleForm = reactive({
+      name: '',
+      phone: '',
+      password: '',
+      school: '',
+      type: '',
+    })
 
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      let type = 1
-      if(ruleForm.type == "学生"){
-        type = 1
-      }else if(ruleForm.type == "教师"){
-        type = 2
-      }
-      register({
-        name: ruleForm.name,
-        phone: ruleForm.phone,
-        password: ruleForm.password,
-        school: ruleForm.school,
-        role: type
-      }).then(res=>{
-        router.push("/")
+    const rules = reactive<FormRules>({
+      name: [
+        { required: true, message: '请输入姓名', trigger: 'blur' }
+      ],
+      phone: [
+        { required: true, message: '请输入手机号', trigger: 'blur' },
+        { min: 11, max: 11, message: '请输入11位手机号', trigger: 'blur' },
+      ],
+      password: [
+        { required: true, message: '请输入密码', trigger: 'blur' }
+      ],
+      school: [
+        { required: true, message: '请输入学校名称', trigger: 'blur' }
+      ],
+      type: [
+        { required: true, message: '必须选择一个类型', trigger: 'blur' }
+      ],
+    })
+
+    const submitForm = async (formEl: FormInstance | undefined) => {
+      if (!formEl) return
+      await formEl.validate((valid, fields) => {
+        if (valid) {
+          let type = 1
+          if(ruleForm.type == "学生"){
+            type = 1
+          }else if(ruleForm.type == "教师"){
+            type = 2
+          }
+          register({
+            name: ruleForm.name,
+            phone: ruleForm.phone,
+            password: ruleForm.password,
+            school: ruleForm.school,
+            role: type
+          }).then(res=>{
+            router.push("/")
+          })
+        }
       })
     }
-  })
-}
+    return {formSize,ruleFormRef,ruleForm,rules,submitForm};
+  }
+})
+
+
 
 </script>
 
