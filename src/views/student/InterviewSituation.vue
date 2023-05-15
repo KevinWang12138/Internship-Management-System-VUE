f18cf567c66e9e90d3b88e96f2814c5dfbeaac9f
KevinWang12138 <wangqianming12138@gmail.com>
2023-05-16 00:35:12 +0800

    查看面试信息连接后端

--- src/views/student/InterviewSituation.vue
+++ src/views/student/InterviewSituation.vue
@@ -16,6 +16,7 @@
 </template>
 <script lang="ts">
 import {defineComponent, reactive} from "vue";
+import { checkInterviewSituation } from "@/request/api";
 const tableData = reactive([
   {
     id: '',
@@ -27,7 +28,41 @@ const tableData = reactive([
 export default defineComponent({
   name:"InterviewSituation",
   setup(){
-
+    while(tableData.length>0){
+      tableData.pop()
+    }
+    //0待审批，1审批中，2面试中，3面试通过，4入职，5不通过
+    checkInterviewSituation().then(res=>{
+      let status = ""
+      for(let i=0;i<res.data.length;i++){
+        switch (res.data[i].status) {
+          case 0:
+            status = "待审批"
+            break;
+          case 1:
+            status = "审批中"
+            break;
+          case 2:
+            status = "面试中"
+            break;
+          case 3:
+            status = "面试通过"
+            break;
+          case 4:
+            status = "入职"
+            break;
+          case 5:
+            status = "不通过"
+            break;
+        }
+        tableData.push({
+          id: res.data[i].id,
+          status: status,
+          jobName: res.data[i].jobName,
+          companyName: res.data[i].companyName,
+        })
+      }
+    })
     return {tableData}
   },
   components:{
