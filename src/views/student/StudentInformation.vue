9223167946e042290d11e36a2e072eb7801a1db1
KevinWang12138 <wangqianming12138@gmail.com>
2023-05-17 20:10:18 +0800

    更新个人信息界面

--- src/views/student/StudentInformation.vue
+++ src/views/student/StudentInformation.vue
@@ -1,109 +1,122 @@
 <template>
-  <el-descriptions
-    title="个人信息"
-    direction="vertical"
-    :column="4"
-    :size="size"
-    border
-  >
-    <el-descriptions-item label="姓名">{{ tableData.studentName }}</el-descriptions-item>
-    <el-descriptions-item label="手机号">{{ tableData.studentPhone }}</el-descriptions-item>
-    <el-descriptions-item label="学号" :span="2">{{ tableData.studentID }}</el-descriptions-item>
-  </el-descriptions>
-  <el-descriptions
-    title="研究信息"
-    direction="vertical"
-    :column="4"
-    :size="size"
-    border
-  >
-    <el-descriptions-item label="学校">{{ tableData.studentSchool }}</el-descriptions-item>
-    <el-descriptions-item label="专业">{{ tableData.studentMajor }}</el-descriptions-item>
-    <el-descriptions-item label="年级">{{ tableData.studentGrade }}</el-descriptions-item>
-    <el-descriptions-item label="研究方向" :span="2">{{ tableData.studentResearchDirection }}</el-descriptions-item>
-  </el-descriptions>
-
-  <el-descriptions
-    title="导师信息"
-    direction="vertical"
-    :column="4"
-    :size="size"
-    border
-  >
-    <el-descriptions-item label="姓名">{{ tableData.teacherName }}</el-descriptions-item>
-    <el-descriptions-item label="手机号">{{ tableData.teacherPhone }}</el-descriptions-item>
-    <el-descriptions-item label="简介" :span="2">{{ tableData.teacherInfo }}</el-descriptions-item>
-  </el-descriptions>
+  <div class="profile">
+    <el-form :model="formData" ref="profileForm" label-width="120px" class="profile-form">
+      <el-form-item label="姓名">
+        <el-input v-model="formData.name" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="年龄">
+        <el-input v-model="formData.age" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-radio-group v-model="formData.gender" :disabled="!editing">
+          <el-radio label="男">男</el-radio>
+          <el-radio label="女">女</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="个人简介">
+        <el-input type="textarea" v-model="formData.bio" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="技术栈">
+        <el-input v-model="formData.techStack" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="研究方向">
+        <el-input v-model="formData.researchArea" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="籍贯">
+        <el-input v-model="formData.hometown" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="身份证号">
+        <el-input v-model="formData.idCardNumber" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="所在地">
+        <el-input v-model="formData.location" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="学校">
+        <el-input v-model="formData.school" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="学号">
+        <el-input v-model="formData.studentId" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="导师姓名">
+        <el-input v-model="formData.tutorName" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="导师手机">
+        <el-input v-model="formData.tutorPhone" :disabled="!editing"></el-input>
+      </el-form-item>
+      <el-form-item label="导师简介">
+        <el-input type="textarea" v-model="formData.tutorBio" :disabled="!editing"></el-input>
+      </el-form-item>
+      <div class="profile-actions">
+        <el-button v-if="!editing" type="primary" @click="editProfile">编辑</el-button>
+        <el-button v-else type="success" @click="saveProfile">保存</el-button>
+        <el-button v-if="editing" type="info" @click="cancelEditing">取消</el-button>
+      </div>
+    </el-form>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive } from "vue";
-import { getStudentInformation } from "@/request/api";
-import { useRouter } from "vue-router";
-function getInformation(){
-  getStudentInformation().then(res=>{
-    tableData.studentName = res.data.studentName
-    tableData.studentPhone = res.data.studentPhone
-    tableData.studentSchool = res.data.studentSchool
-    tableData.teacherName = res.data.teacherName
-    tableData.teacherPhone = res.data.teacherPhone
+<script>
+import { reactive, ref } from 'vue';
 
-    tableData.studentID=res.data.studentID
-    tableData.studentMajor=res.data.studentMajor
-    if(res.data.studentGrade == 1){
-      tableData.studentGrade = "研一"
-    }else if(res.data.studentGrade == 2){
-      tableData.studentGrade = "研二"
-    }else if(res.data.studentGrade == 3){
-      tableData.studentGrade = "研三"
-    }else if(res.data.studentGrade == 4){
-      tableData.studentGrade = "研四"
-    }
-    tableData.studentResearchDirection=res.data.studentResearchDirection
-    tableData.teacherInfo= res.data.teacherInfo
-  })
-}
-const tableData = reactive(
-  {
-    studentPhone: '',
-    studentName: '',
-    studentSchool: '',
-    studentID:'',
-    studentMajor:'',
-    studentGrade:'',
-    studentResearchDirection:'',
+export default {
+  name: 'ProfilePage',
+  setup() {
+    const formData = reactive({
+      name: 'John Doe',
+      age: 25,
+      gender: '男',
+      bio: '个人简介',
+      techStack: '技术栈',
+      researchArea: '研究方向',
+      hometown: '籍贯',
+      idCardNumber: '身份证号',
+      location: '所在地',
+      school: '学校',
+      studentId: '学号',
+      tutorName: '导师姓名',
+      tutorPhone: '导师手机',
+      tutorBio: '导师简介',
+    });
 
-    teacherPhone: "",
-    teacherName: '',
-    teacherInfo: ''
-  }
-)
-export default defineComponent({
-  name:"StudentInformation",
-  setup(){
-    const router = useRouter()
-    function edit(){
-      router.push('/teacher/edit')
-    }
-    getInformation()
-    return {tableData,edit}
+    const editing = ref(false);
+
+    const editProfile = () => {
+      editing.value = true;
+    };
+
+    const saveProfile = () => {
+      // Perform saving logic here
+      editing.value = false;
+    };
+
+    const cancelEditing = () => {
+      editing.value = false;
+    };
+
+    return {
+      formData,
+      editing,
+      editProfile,
+      saveProfile,
+      cancelEditing,
+    };
   },
-  components:{
-
-  },
-})
+};
 </script>
 
 <style scoped>
-.el-descriptions {
-  margin-top: 20px;
+.profile {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
 }
-.editButton{
-  position: absolute;
-  top: 0;
-  right: 50px;
-  width: 200px;
-  height: 50px;
-  font-size: 20px;
+
+.profile-form {
+  width: 100%;
+}
+
+.profile-actions {
+  text-align: right;
+  margin-top: 20px;
 }
 </style>
